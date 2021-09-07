743b92ba3fd33e26b17a7a407068f68305c353e3
Devang506 <84566116+Devang506@users.noreply.github.com>
2021-09-07 21:03:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// JAVASCRIPT CODE
 // Detecting button press
  var numOfButtons = document.querySelectorAll(".drum").length;
  for(var i =0; i<numOfButtons;i++){
@@ -57,6 +59,7 @@ default : console.log(buttonInnerHtml);
 
   }
 }
+// creating animation after button get pressed
 function buttonAnimation(currentkey){
              var activeButton  = document.querySelector("."+currentkey);
              activeButton.classList.add("pressed");
@@ -68,3 +71,76 @@ function buttonAnimation(currentkey){
 
 
 }
+
+
+
+
+// jQuery Code
+//  var numOfButtons = $(".drum").length;
+//  for(var i =0; i<numOfButtons;i++){
+//   $(".drum")[i].click(functionn(){
+//      var buttonInnerHtml = this.innerHTML;
+//     makeSound(buttonInnerHtml);
+//     buttonAnimation(buttonInnerHtml);
+//    }
+//  )
+// }
+// $.keydown(function(event){
+//   makeSound(event.key);
+//   buttonAnimation(event.key);
+// })
+// function makeSound(key)
+// {
+//   switch (key) {
+
+//     case "w":
+//       var tom1 = new Audio("sounds/tom-1.mp3");
+//       tom1.play();
+//       break;
+
+//       case "a":
+//         var    tom2 = new Audio("sounds/tom-2.mp3");
+//         tom2.play();
+//         break;
+
+//         case "s":
+//           var   tom3 = new Audio("sounds/tom-3.mp3");
+//           tom3.play();
+//           break;
+
+//           case "d":
+//             var    tom4 = new Audio("sounds/tom-4.mp3");
+//               tom4.play();
+//             break;
+
+//            case "j":
+//               var    crash = new Audio("sounds/crash.mp3");
+//               crash.play();
+//               break;
+
+//               case "k":
+//                 var kick = new Audio("sounds/kick-bass.mp3");
+//                 kick.play();
+//                 break;
+
+//                 case "l":
+//                   var snare = new Audio("sounds/snare.mp3");
+//                   snare.play();
+//                   break;
+
+// default : console.log(buttonInnerHtml);
+
+//   }
+// }
+// // creating animation after button get pressed
+// function buttonAnimation(currentkey){
+//              var activeButton  = $("."+currentkey);
+//              activeButton.addClass("pressed");
+
+//              setTimeout(function(){
+//                activeButton.removeClass("pressed");
+//              },100);
+
+
+
+// }
